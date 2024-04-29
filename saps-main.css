2243bcd9ae7708fe240db98baf92cdfac12faff4
UuCats1 <132372826+UuCats1@users.noreply.github.com>
2024-04-30 01:53:16 +0800

    Add files via upload

---
+++ saps-main.css
@@ -0,0 +1,80 @@
+/* Reset styles */
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+/* Container */
+
+.container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background-color: #111;
+  color: #fff;
+}
+
+/* Header */
+
+header {
+  width: 100%;
+  height: 70px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #222;
+}
+
+header h1 {
+  margin-left: 20px;
+  font-size: 2rem;
+}
+
+header nav {
+  margin-right: 20px;
+}
+
+header nav a {
+  margin-right: 20px;
+  font-size: 1.2rem;
+}
+
+/* Main */
+
+main {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+}
+
+/* Sections */
+
+section {
+  padding: 20px;
+}
+
+section h2 {
+  font-size: 2rem;
+}
+
+section p {
+  font-size: 1.2rem;
+}
+
+/* Footer */
+
+footer {
+  width: 100%;
+  height: 70px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #222;
+}
+
+footer p {
+  font-size: 1.2rem;
+}
